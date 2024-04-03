d9981ec53582dd9b5d407cb44ea9acb0a40b1d84
mak <makpham@udel.edu>
2024-04-03 12:44:01 -0400

    Makayla added name

--- src/App.tsx
+++ src/App.tsx
@@ -31,6 +31,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>
+          Makayla Pham
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
