59b407828d3ba72dfba2eb7198fc82ca4a5562c7
mak <makpham@udel.edu>
2024-04-06 13:48:52 -0400

    changed back

--- src/App.tsx
+++ src/App.tsx
@@ -45,7 +45,7 @@ function App() {
       </header>
       <Form>
         <Form.Label>API Key:</Form.Label>
-        <Form.Control type="password" placeholder="Insert API Key Here"></Form.Control>
+        <Form.Control type="password" placeholder="Insert API Key Here" onChange={changeKey}></Form.Control>
         <br></br>
         <Button className="Submit-Button" onClick={handleSubmit}>Submit</Button>
       </Form>
