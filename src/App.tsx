9acd1034d1f52bcf12903bebd7b24b33755b835e
mak <makpham@udel.edu>
2024-04-07 10:15:32 -0400

    put api key form in footer

--- src/App.tsx
+++ src/App.tsx
@@ -43,12 +43,14 @@ function App() {
           Learn React
         </a>
       </header>
-      <Form>
-        <Form.Label>API Key:</Form.Label>
-        <Form.Control type="password" placeholder="Insert API Key Here"></Form.Control>
-        <br></br>
-        <Button className="Submit-Button" onClick={handleSubmit}>Submit</Button>
-      </Form>
+      <footer className="App-footer">
+        <Form>
+          <Form.Label>API Key:</Form.Label>
+          <Form.Control type="password" placeholder="Insert API Key Here" onChange={changeKey}></Form.Control>
+          <br></br>
+          <Button className="Submit-Button" onClick={handleSubmit}>Submit</Button>
+        </Form>
+      </footer>
     </div>
   );
 }
