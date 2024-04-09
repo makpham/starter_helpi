b6196072b7b0ed76f64f43737b43ca7155066ff0
Trung Nguyen <trungtng@udel.edu>
2024-04-09 11:53:32 -0400

    added components folder and updated imports

--- src/components/Option.tsx
+++ src/components/Option.tsx
@@ -1,7 +1,3 @@
-// import React, { useState } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-// import { Button, Form } from 'react-bootstrap';
 import './Option.css'
 interface OptionProps{
     OptionTitle: string;
