57c295346990ebf32dd84c0ebadebc3ade1aa4cd
Trung Nguyen <trungtng@udel.edu>
2024-04-09 11:17:45 -0400

    added options component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Button, Form } from 'react-bootstrap';
+import { Option } from './Option';
 
 //local storage and API Key: key should be entered in by the user and will be stored in local storage (NOT session storage)
 let keyData = "";
@@ -49,6 +50,7 @@ function App() {
         <br></br>
         <Button className="Submit-Button" onClick={handleSubmit}>Submit</Button>
       </Form>
+      <Option OptionDescription='' OptionTitle='Detailed Questions' OptionHeight='30em'/>
     </div>
   );
 }
