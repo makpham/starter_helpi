116c20fea651fe2e75e36a1f6418d2a6b4af2c58
Trung Nguyen <trungtng@udel.edu>
2024-04-11 07:45:28 -0400

    added proper routing, added code to their respective pages files, restyled option

--- src/components/Option.tsx
+++ src/components/Option.tsx
@@ -1,15 +1,15 @@
-import './Option.css'
-interface OptionProps{
-    OptionTitle: string;
-    OptionDescription: string;
-    onClick: () => void;
-};
+import "./Option.css";
+interface OptionProps {
+  OptionTitle: string;
+  OptionDescription: string;
+  onClick: () => void;
+}
 
-export function Option(prop: OptionProps){
-    return <div onClick={prop.onClick} className='container'>
-            <h1 className='title'>{prop.OptionTitle}</h1>
-            <p className='description'>
-                {prop.OptionDescription}
-            </p>
+export function Option(prop: OptionProps) {
+  return (
+    <div onClick={prop.onClick} className="container">
+      <h1 className="title">{prop.OptionTitle}</h1>
+      <p className="description">{prop.OptionDescription}</p>
     </div>
+  );
 }
