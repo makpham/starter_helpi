55e7b3739864d9372e3737afc55dfb8bf59d41ab
Trung Nguyen <trungtng@udel.edu>
2024-04-11 00:48:55 -0400

    removed original template components

--- src/App.tsx
+++ src/App.tsx
@@ -50,21 +50,21 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
+        </p> */}
+        {/* <p>
           Makayla Pham, Trung Nguyen, Jared Miller, Araf Jahin
-        </p>
-        <a
+        </p> */}
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
 
       <body className='App-body'>
