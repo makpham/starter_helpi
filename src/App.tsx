7af1edb8a4f138c4cdb1e6721c46d73f9be2ed6e
Jared <jem219@icloud.com>
2024-04-11 12:02:32 -0400

    new

--- src/App.tsx
+++ src/App.tsx
@@ -37,12 +37,23 @@ function App() {
   }
 
   return (
-    <HashRouter>
-      <header>
-        <Link to="/"><img src={img} className="logo" alt="404"/></Link>
-        <h1 className="site-name">Starter Helpi</h1>
-        <MenuBar items={menuItems} />
-
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <p>
+          Makayla Pham, Trung Nguyen, Jared Miller, Araf Jahin
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
