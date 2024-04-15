0b96f658fa6bce9454a2d6f76efcb536d373cbba
mak <makpham@udel.edu>
2024-04-14 23:54:23 -0400

    Revert "new"
    This reverts commit 7af1edb8a4f138c4cdb1e6721c46d73f9be2ed6e.

--- src/App.tsx
+++ src/App.tsx
@@ -37,23 +37,12 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          Makayla Pham, Trung Nguyen, Jared Miller, Araf Jahin
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <HashRouter>
+      <header>
+        <Link to="/"><img src={img} className="logo" alt="404"/></Link>
+        <h1 className="site-name">Starter Helpi</h1>
+        <MenuBar items={menuItems} />
+
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
