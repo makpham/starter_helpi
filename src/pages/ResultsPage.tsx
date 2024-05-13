e4a0852c8efbd3e0f22df383a5fb8c9f9c49112c
Trung Nguyen <trungtng@udel.edu>
2024-05-13 00:10:51 -0400

    started working on results

--- src/pages/ResultsPage.tsx
+++ src/pages/ResultsPage.tsx
@@ -1,10 +1,22 @@
 import "./ResultsPage.css";
+import Typewriter from 'typewriter-effect';
 
 
 function ResultsPage({ results, setResults }: { results: string, setResults: React.Dispatch<React.SetStateAction<string>> }) {
   
   return (
     <div id="results-body">
+        <h1>
+            <Typewriter
+                options={{
+                    delay: 50
+                }}
+                onInit={(typewriter) => {
+                    typewriter.typeString('Congratulations! You’ve successfully completed the questionnaire! Now, let’s dive into the results. Below, you’ll find a summary of your answers and insights:')
+                        .start();
+                }}
+            />
+        </h1>
     </div>
   );
 };
