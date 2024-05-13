5904ca45fbc66b3f36656dd425116aeb3d2fdc3c
Jared Miller <jem219@icloud.com>
2024-05-13 16:13:57 -0400

    pedals now go behind text

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -96,7 +96,7 @@ function Home() {
   return (
     <div id='home-body'>
       <CherryBlossom />
-      <div id='welcome-text' className='Sofia'>
+      <div id='welcome-text'>
         <h1 id='landing-title'>
           Hello Future 
           <Typewriter
