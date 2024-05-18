748616a93df736917b5d229c4f326735aceb28b0
Jared Miller <jem219@icloud.com>
2024-05-18 17:04:06 -0400

    commented Modal

--- src/components/RedirectModal.tsx
+++ src/components/RedirectModal.tsx
@@ -2,20 +2,24 @@ import React from 'react';
 import { Modal, Button } from 'react-bootstrap';
 import './RedirectModal.css';
 
+// Define the properties for the RedirectModal component
 interface RedirectModalProps {
-  show: boolean;
-  handleCancel: () => void;
-  handleConfirm: () => void;
+  show: boolean; // Whether or not the modal should be shown
+  handleCancel: () => void; // Called when cancel button is clicked
+  handleConfirm: () => void; // Called when confirm button is clicked
 }
 
+// The RedirectModal displays a confirmation modal when the user is about to switch questionnaires
 const RedirectModal: React.FC<RedirectModalProps> = ({ show, handleCancel, handleConfirm }) => {
   return (
     <Modal show={show} onHide={handleCancel}>
       <Modal.Header closeButton>
         <Modal.Title>Confirmation</Modal.Title>
       </Modal.Header>
-      <Modal.Body>Switching to a different questionnaire will erase your responses. 
-        Are you sure you want to proceed?</Modal.Body>
+      <Modal.Body>
+        Switching to a different questionnaire will erase your responses. 
+        Are you sure you want to proceed?
+      </Modal.Body>
       <Modal.Footer>
         <Button variant="secondary" onClick={handleCancel}>
           Cancel
